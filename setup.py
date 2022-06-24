522d99d436925639a2f93c357ce871335ac39b8a
ketanGangal <ketangangal98@gmail.com>
2022-06-24 17:36:46 +0530

    added setup

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-l = ["skills", "exp"]
+from setuptools import setup, find_packages
+
+setup(
+    name="NER",
+    version="0.0.1",
+    author="Ketan Gangal",
+    author_email="ketangangal98@gmail.com",
+    packages=find_packages()
+)
